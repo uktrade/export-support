0d0ae5177ec1a443acf43939c51fe730a09580d1
Kevin Carrogan <kevin.carrogan@gmail.com>
2021-06-16 15:56:32 +0100

    Add import enquiries page

--- export_support/core/urls.py
+++ export_support/core/urls.py
@@ -5,4 +5,7 @@ from export_support.core import views
 app_name = "core"
 urlpatterns = [
     path("", views.EnquirySubjectFormView.as_view(), name="index"),
+    path(
+        "import-enquiries", views.ImportEnquiriesView.as_view(), name="import-enquiries"
+    ),
 ]
--- export_support/core/views.py
+++ export_support/core/views.py
@@ -1,8 +1,24 @@
-from django.views.generic import FormView
+from django.shortcuts import redirect
+from django.views.generic import FormView, TemplateView
 
-from .forms import EnquirySubjectForm
+from .forms import EnquirySubjectChoices, EnquirySubjectForm
 
 
 class EnquirySubjectFormView(FormView):
     form_class = EnquirySubjectForm
     template_name = "core/enquiry_subject.html"
+
+    def form_valid(self, form):
+        enquiry_subject_value = form.cleaned_data["enquiry_subject"]
+
+        only_importing = enquiry_subject_value == [
+            EnquirySubjectChoices.IMPORTING_GOODS_TO_THE_UK
+        ]
+        if only_importing:
+            return redirect("core:import-enquiries")
+
+        return super().form_valid(form)
+
+
+class ImportEnquiriesView(TemplateView):
+    template_name = "core/import_enquiries.html"
