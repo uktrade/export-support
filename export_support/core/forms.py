ac4aef496327bd6dae50f492f96466d941bf20bc
Kevin Carrogan <kevin.carrogan@gmail.com>
2021-07-08 18:24:42 +0100

    Remove unused custom form field inputs

--- export_support/core/forms.py
+++ export_support/core/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.utils.safestring import mark_safe
 
 from export_support.gds import fields as gds_fields
 
--- export_support/gds/fields.py
+++ export_support/gds/fields.py
@@ -7,15 +7,3 @@ class CheckboxSelectMultiple(forms.CheckboxSelectMultiple):
 
 class RadioSelect(forms.RadioSelect):
     template_name = "gds/forms/fields/radio_select.html"
-
-
-class TextInput(forms.TextInput):
-    template_name = "gds/forms/fields/text_input.html"
-
-
-class EmailInput(forms.EmailInput):
-    template_name = "gds/forms/fields/email_input.html"
-
-
-class Textarea(forms.Textarea):
-    template_name = "gds/forms/fields/textarea.html"
