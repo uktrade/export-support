ab3b3dceb614103d91a11c0ead89506261bbe0f6
Kevin Carrogan <kevin.carrogan@gmail.com>
2021-07-21 13:11:29 +0100

    Add directory forms API healthcheck to healthcheck

--- export_support/healthcheck/views.py
+++ export_support/healthcheck/views.py
@@ -1,5 +1,7 @@
 import time
+import urllib
 
+from django.conf import settings
 from django.views.generic import TemplateView
 
 
@@ -10,6 +12,12 @@ class HealthCheckView(TemplateView):
     def get_context_data(self, **kwargs):
         """Adds status and response time to response context"""
         context = super().get_context_data(**kwargs)
+
+        try:
+            urllib.request.urlopen(settings.DIRECTORY_FORMS_API_HEALTHCHECK_URL)
+        except Exception as e:
+            raise Exception("Directory forms API healthcheck failed") from e
+
         context["status"] = "OK"
         # nearest approximation of a response time
         context["response_time"] = time.time() - self.request.start_time
