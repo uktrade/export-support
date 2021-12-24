1f3b879a7a620049d0ac6ba65f807f9081acb276
Kevin Carrogan <kevin.carrogan@gmail.com>
2021-12-24 11:12:52 +0000

    Add marketing consent to zendesk output

--- export_support/core/tests/test_views/test_enquiry_wizard.py
+++ export_support/core/tests/test_views/test_enquiry_wizard.py
@@ -282,6 +282,7 @@ def test_full_steps_private_or_limited_business_type_wizard_success(
             "other_sector": "ANOTHER SECTOR",
             "sectors": "Advanced engineering, Aerospace, Agriculture, horticulture, fisheries and pets, Airports, Automotive, Chemicals, Construction, Consumer and retail, Creative industries, Defence, Education and training, Energy, Environment, Financial and professional services, Food and drink, Healthcare services, Logistics, Maritime, Medical devices and equipment, Mining, Pharmaceuticals and biotechnology, Railways, Security, Space, Sports economy, Technology and smart cities, Water",  # noqa: E501
             "how_did_you_hear_about_this_service": "Search engine",
+            "marketing_consent": True,
             "_custom_fields": None,
         }
     )
@@ -515,6 +516,7 @@ def test_full_steps_other_organisation_business_type_wizard_success(
             "other_sector": "ANOTHER SECTOR",
             "sectors": "Advanced engineering, Aerospace, Agriculture, horticulture, fisheries and pets, Airports, Automotive, Chemicals, Construction, Consumer and retail, Creative industries, Defence, Education and training, Energy, Environment, Financial and professional services, Food and drink, Healthcare services, Logistics, Maritime, Medical devices and equipment, Mining, Pharmaceuticals and biotechnology, Railways, Security, Space, Sports economy, Technology and smart cities, Water",  # noqa: E501
             "how_did_you_hear_about_this_service": "Search engine",
+            "marketing_consent": True,
             "_custom_fields": None,
         }
     )
@@ -746,6 +748,7 @@ def test_full_steps_solo_exporter_business_type_wizard_success(
             "other_sector": "ANOTHER SECTOR",
             "sectors": "Advanced engineering, Aerospace, Agriculture, horticulture, fisheries and pets, Airports, Automotive, Chemicals, Construction, Consumer and retail, Creative industries, Defence, Education and training, Energy, Environment, Financial and professional services, Food and drink, Healthcare services, Logistics, Maritime, Medical devices and equipment, Mining, Pharmaceuticals and biotechnology, Railways, Security, Space, Sports economy, Technology and smart cities, Water",  # noqa: E501
             "how_did_you_hear_about_this_service": "Search engine",
+            "marketing_consent": True,
             "_custom_fields": None,
         }
     )
@@ -965,6 +968,7 @@ def test_full_steps_private_or_limited_business_type_wizard_success_custom_field
             "other_sector": "ANOTHER SECTOR",
             "sectors": "Advanced engineering, Aerospace, Agriculture, horticulture, fisheries and pets, Airports, Automotive, Chemicals, Construction, Consumer and retail, Creative industries, Defence, Education and training, Energy, Environment, Financial and professional services, Food and drink, Healthcare services, Logistics, Maritime, Medical devices and equipment, Mining, Pharmaceuticals and biotechnology, Railways, Security, Space, Sports economy, Technology and smart cities, Water",  # noqa: E501
             "how_did_you_hear_about_this_service": "Search engine",
+            "marketing_consent": False,
             "_custom_fields": [
                 {
                     "222": [
@@ -1244,6 +1248,7 @@ def test_full_steps_other_organisation_business_type_wizard_success_custom_field
             "other_sector": "ANOTHER SECTOR",
             "sectors": "Advanced engineering, Aerospace, Agriculture, horticulture, fisheries and pets, Airports, Automotive, Chemicals, Construction, Consumer and retail, Creative industries, Defence, Education and training, Energy, Environment, Financial and professional services, Food and drink, Healthcare services, Logistics, Maritime, Medical devices and equipment, Mining, Pharmaceuticals and biotechnology, Railways, Security, Space, Sports economy, Technology and smart cities, Water",  # noqa: E501
             "how_did_you_hear_about_this_service": "Search engine",
+            "marketing_consent": False,
             "_custom_fields": [
                 {
                     "222": [
@@ -1522,6 +1527,7 @@ def test_full_steps_solo_exporter_business_type_wizard_success_custom_fields(
             "other_sector": "ANOTHER SECTOR",
             "sectors": "Advanced engineering, Aerospace, Agriculture, horticulture, fisheries and pets, Airports, Automotive, Chemicals, Construction, Consumer and retail, Creative industries, Defence, Education and training, Energy, Environment, Financial and professional services, Food and drink, Healthcare services, Logistics, Maritime, Medical devices and equipment, Mining, Pharmaceuticals and biotechnology, Railways, Security, Space, Sports economy, Technology and smart cities, Water",  # noqa: E501
             "how_did_you_hear_about_this_service": "Search engine",
+            "marketing_consent": False,
             "_custom_fields": [
                 {
                     "222": [
@@ -1798,6 +1804,7 @@ def test_full_steps_wizard_success_private_custom_fields_are_ignored(
             "other_sector": "ANOTHER SECTOR",
             "sectors": "Advanced engineering, Aerospace, Agriculture, horticulture, fisheries and pets, Airports, Automotive, Chemicals, Construction, Consumer and retail, Creative industries, Defence, Education and training, Energy, Environment, Financial and professional services, Food and drink, Healthcare services, Logistics, Maritime, Medical devices and equipment, Mining, Pharmaceuticals and biotechnology, Railways, Security, Space, Sports economy, Technology and smart cities, Water",  # noqa: E501
             "how_did_you_hear_about_this_service": "Search engine",
+            "marketing_consent": False,
             "_custom_fields": [
                 {"222": CompanyTurnoverChoices.BELOW_85000},
             ],
